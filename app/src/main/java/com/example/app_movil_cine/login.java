509f2e72a79adbcba0fcd0ab6f0be3b2e1d424c5
Keyron13 <aguilarortizkeirodamian@gmail.com>
2023-07-14 00:15:14 -0500

    reajustes al registro dinales

--- app/src/main/java/com/example/app_movil_cine/login.java
+++ app/src/main/java/com/example/app_movil_cine/login.java
@@ -2,7 +2,10 @@ package com.example.app_movil_cine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        TextView linkregistro;
+
+        linkregistro = findViewById(R.id.linkRegistro);
+
+        linkregistro.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), registro.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
