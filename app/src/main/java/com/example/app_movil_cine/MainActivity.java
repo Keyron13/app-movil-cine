4b7d432bf6de689f68e8d60a3e23bd9d433f2495
Keyron13 <aguilarortizkeirodamian@gmail.com>
2023-07-17 22:06:16 -0500

    creacion del menu

--- app/src/main/java/com/example/app_movil_cine/MainActivity.java
+++ app/src/main/java/com/example/app_movil_cine/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.app_movil_cine;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new page_home()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId()==R.id.nav_home){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new page_home()).commit();
+        } else if (item.getItemId()==R.id.nav_search){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new page_busqueda()).commit();
+        } else if (item.getItemId()==R.id.nav_users){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new page_perfil()).commit();
+        } else if (item.getItemId()==R.id.nav_favorite){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new page_favoritos()).commit();
+        } else if (item.getItemId()==R.id.nav_news){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new page_noticias()).commit();
+        } else if (item.getItemId()==R.id.nav_logout){
+            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/app_movil_cine/page_favoritos.java
@@ -0,0 +1,20 @@
+package com.example.app_movil_cine;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class page_favoritos extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.page_favoritos,container,false) ;
+    }
+}
